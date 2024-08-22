1ead4132a947f704d2732efdf7998ce87503146a
arpitarikibe23 <arpitarikibe23@gmailcom>
2024-08-22 22:59:12 +0530

    initial commit

---
+++ StudentManagement-System-dbms-miniproject-main/StudentManagement-System-dbms-miniproject-main/student management/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+
+        # this is method 2 to save data in db
+        newuser=User(username=username,email=email,password=password)
+        db.session.add(newuser)
+        db.session.commit()
+        flash("Signup Succes Please Login","success")
+        return render_template('login.html')
+
+          
+
+    return render_template('signup.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == "POST":
